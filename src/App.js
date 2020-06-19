2174b211764147002c155a3ce7c98640b7a9ef3a
Pankhuri <pankhuritrikha@gmail.com>
2020-06-19 13:10:23 +0530

    Search on writing the complete query in the search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -6,6 +7,9 @@ const App = () => {
   //get the keys
    const APP_ID = "dd12db1d";
    const APP_KEY = "6bae481969a0cc1cc84decc54f1dacc6";
+
+   const [recipes, setRecipes] = useState([]);
+   const [search, setSearch] = useState("");
 
    //useEffect takes an arrow function
    useEffect(() => {
@@ -18,17 +22,30 @@ const App = () => {
       const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
        // whatever is after the q (that is query) is what we are searching for
       const data = await response.json(); // awaits does the work of promises
-      console.log(data);
+      setRecipes(data.hits);
+      console.log(data.hits);
+  }
+
+  const UpdateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
   }
 
   return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" value={search} onChange={UpdateSearch} />
         <button className="search-button" type="submit">
           Search
         </button>
        </form>
+        {recipes.map((recipe) => (
+            <Recipe key={recipe.recipe.label} 
+                    title={recipe.recipe.label} 
+                    calories={recipe.recipe.calories} 
+                    image={recipe.recipe.image}
+            />
+        ))}
     </div>
   );
 }
